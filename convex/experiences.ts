dbcddedacedacb9b582a96e3f81b6d4ed7cf3e9a
Razi <syedru@mcmaster.ca>
2025-05-06 11:24:44 -0400

    sort by descending date

--- convex/experiences.ts
+++ convex/experiences.ts
@@ -3,6 +3,11 @@ import { query } from "./_generated/server";
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query("experiences").collect();
+    const experiences = await ctx.db.query("experiences").collect();
+    // Sort by crossingDate descending
+    return experiences.sort(
+      (a, b) =>
+        new Date(b.crossingDate).getTime() - new Date(a.crossingDate).getTime()
+    );
   },
 });
